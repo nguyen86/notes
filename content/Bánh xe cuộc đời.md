cfae77f62b3c6b13ed50668650c20dd70f598d37
nguyen86 <nguyendaodinh@gmail.com>
2024-03-12 08:45:22 +0700

    init

---
+++ content/Bánh xe cuộc đời.md
@@ -0,0 +1,26 @@
+---
+title: Bánh xe cuộc đời
+draft: false
+tags:
+  - example-tag
+---
+
+# Tiền/tài sản
+
+1. Nâng chuẩn lên, thu nhập 10 triệu, 20 triệu, 30 triệu, 50 triệu
+2. Mây tầng nào hút mây tầng đó, tư duy tầm nào, 5 năm thu nhập không quá 20 triệu, cần nâng cấp bản thân trước.
+3. Nếu bạn khong đi làm trong 6 tháng tới thì sẽ như thế nào? Có an toàn không?|
+
+# Mối quan hệ xã hội
+
+1. Bạn có MQH chất lượng không
+2. Trong 60 ngày gần đây có gặp ai liên tiếp mà khiến cho cuộc sống của bạn trở nên tốt hơn chưa?
+3. MQH số lượng nhiều (facebook 5000 người) nhưng liệu có chất lượng ko?
+4. Bạn có thường thăm bạn cấp 3, ĐH của mình không?
+5. Bạn có nhiều bạn bè và người thân gần bên cạnh và thường xuyên giữ liên lạc với họ không?
+6. Bạn có thường xuyên dành thời gian để tìm hiểu và hiểu biết thêm về người khác không?
+7. Bạn có cảm thấy thoải mái và tự tin khi giao tiếp và tương tác với người khác không?
+8. Bạn có cảm thấy rằng bạn luôn có thể dựa vào những người xung quanh để được hỗ trợ và giúp đỡ khi cần thiết không?
+9. Giả sử chẳng may bạn ốm thì có bao nhiêu người sẵn sàng bỏ chồng con ở nhà để vào viện chăm bạn?
+10. Bạn có thường lắng nghe và tương tác với người khác để học hỏi và trao đổi kinh nghiệm không?
+11. Bạn có cảm thấy mình được đánh giá cao và được người khác tôn trọng không?
---
+++ quartz.config.ts
@@ -0,0 +1,79 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "👩‍💻 Ellie's Notes",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    baseUrl: "ellie.wtf",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#f5f2e9",
+          lightgray: "#e0dcd3",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#384b73",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+        darkMode: {
+          light: "#1a1915",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#5d7da5",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.TableOfContents(),
+      Plugin.CreatedModifiedDate({
+        // you can add 'git' here for last modified from Git
+        // if you do rely on git for dates, ensure defaultDateType is 'modified'
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
